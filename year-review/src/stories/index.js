44db970a0dd68a91eb7fabff1317723e63d3253a
wenddyywang <www2105@columbia.edu>
2019-04-14 16:49:43 -0400

    Created GenericPanelLayout component

--- year-review/src/stories/index.js
+++ year-review/src/stories/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-import Article from '../components/Article.js';
 
 import NavigationItems from '../Component/Navigation/NavigationItems'
+import GenericPanelLayout from '../Component/GenericPanelLayout'
 
-storiesOf('Article', module).add('Article', () => <Article title="title" author="author"/>);
+storiesOf('GenericPanelLayout', module).add("GenericPanelLayout",() => <GenericPanelLayout />)
