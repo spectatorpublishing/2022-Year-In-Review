8a7f574c37213181fc076300ae2b71ec77b24825
stacytao <stacyytao@gmail.com>
2019-04-16 21:23:35 -0400

    Add Global Styles and navbar (currently broken)

--- year-review/src/stories/index.js
+++ year-review/src/stories/index.js
@@ -3,8 +3,47 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-import Article from '../components/Article.js';
 
 import NavigationItems from '../Component/Navigation/NavigationItems'
+import NavBar from '../Component/Navigation/NavBar'
+import { MemoryRouter } from 'react-router';
 
-storiesOf('Article', module).add('Article', () => <Article title="title" author="author"/>);
+let sampleMenuItems = [
+  {
+    "name": "Explore",
+    "link": "/explore",
+    "external": false
+  },
+  {
+    "name": "Whiteboard",
+    "link": "/whiteboard",
+    "external": false
+  },
+  {
+    "name": "FAQ",
+    "link": "/faq",
+    "external": false
+  },
+  {
+    "name": "Spectrum",
+    "link": "https://www.columbiaspectator.com/spectrum/",
+    "external": true
+  }
+];
+  
+storiesOf('NavBar', module)
+  .addDecorator(story => (
+    <MemoryRouter initialEntries={['/']}>
+      <div>
+        {story()}
+        <p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p>
+        <p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p>
+        <p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p>
+        <p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p>
+        <p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p>
+        <p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p><p>filler</p>
+      </div>
+    </MemoryRouter>
+  ))
+  .add('navbar', () => <NavBar menuItems={sampleMenuItems} />)
+  .add('fixed navbar', () => <NavBar menuItems={sampleMenuItems} fixed />);
