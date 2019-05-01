a616f677f5cd6440db9f657f5677e90191926efc
Kevin Li <kevinl94303spam@gmail.com>
2019-04-30 21:29:46 -0400

    wrapped router items in news in a div

--- year-review/src/containers/NewsContainer.js
+++ year-review/src/containers/NewsContainer.js
@@ -59,17 +59,19 @@ class NewsContainer extends Component {
         
     return (
       <MemoryRouter initialEntries={['/']}>
-      <div onWheel = {this.handleWheel}>
-       
-        <PageIntro title1="News" text1="blurb" title2="title" text2="blurb" img_src={this.props.intro_img} />
-        <ScrollingNavBar menuItems={this.props.NavItems}>
-          {this.props.SliderData.map((data, i) => 
-            <GenericPanelLayout key={i} data = {this.props.SliderData[i]} />)}
-        </ScrollingNavBar>
+      <div>
+        <div onWheel = {this.handleWheel}>
         
-        
+          <PageIntro title1="News" text1="blurb" title2="title" text2="blurb" img_src={this.props.intro_img} />
+          <ScrollingNavBar menuItems={this.props.NavItems}>
+            {this.props.SliderData.map((data, i) => 
+              <GenericPanelLayout key={i} data = {this.props.SliderData[i]} />)}
+          </ScrollingNavBar>
+          
+          
+        </div>
+        <Footer title1="News" text1="blurb" title2="title" text2="blurb" img_src={this.props.intro_img} />
       </div>
-      <Footer title1="News" text1="blurb" title2="title" text2="blurb" img_src={this.props.intro_img} />
       </MemoryRouter>
       
     );
