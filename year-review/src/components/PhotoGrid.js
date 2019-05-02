6527a7128375cb5eea37c0f28cfc30d81c4ab86c
Yunsu <yk2762@columbia.edu>
2019-05-01 23:24:21 -0400

    image box mobile doesn't work

--- year-review/src/components/PhotoGrid.js
+++ year-review/src/components/PhotoGrid.js
@@ -1,19 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
 import ImageBox from './ImageBox/ImageBox'
+import { MobileAndTablet, Desktop } from 'react-responsive-simple';
 
 const Grid = styled.div`
     display: grid;
-    grid-template-columns: 25% 25% 25% 25%;
-    grid-template-rows: 50% 50%; 
-    grid-column-gap: 0px;
-    grid-row-gap: 0px;
+
+    @media only screen and (min-width:992px){
+        grid-template-columns: 25% 25% 25% 25%;
+        grid-auto-rows: 50%; 
+        grid-column-gap: 0px;
+        grid-row-gap: 0px;
+    }
+
+    @media only screen and (max-width: 991px){
+        grid-template-columns: 50% 50%;
+        grid-auto-rows: 50vw; 
+        grid-column-gap: 3px;
+        grid-row-gap: 3px;
+    }
+
 `;
 
 function PhotoGrid(props){
     const grid = props.data.map((data, i) => <ImageBox key={i} data={data}/>);
     return (
-        <Grid>{grid}</Grid>
+		<div>
+			<Grid>{grid}</Grid>
+		</div>
     ); 
  
 }
