00aa61e600d3b216a78e4f2781df0e904f93ccea
stacytao <stacyytao@gmail.com>
2019-05-02 13:01:07 -0400

    Make sure columnist formatting works

--- year-review/src/containers/OpinionContainer.js
+++ year-review/src/containers/OpinionContainer.js
@@ -52,10 +52,23 @@ export default class OpinionContainer extends Component {
           <TempLetter> LETTER FROM THE EDITOR </TempLetter>
           <ExpandingColumns data = {opinion_data}/>
           <ScrollingNavBar menuItems={opinion_data}>
-            <div style={{background: "url(https://images.unsplash.com/photo-1466129646777-494b376a670c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80)", width: "100vw", height: "100vh",}}></div>
-            <div style={{background: "url(https://images.unsplash.com/photo-1507608869274-d3177c8bb4c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80)", width: "100vw", height: "100vh",}}></div>
-            <div style={{background: "url(https://images.unsplash.com/photo-1479030160180-b1860951d696?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)", width: "100vw", height: "100vh",}}></div>
-            <div style={{background: "url(https://images.unsplash.com/photo-1520634996521-d0985c84316a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjI0MX0&auto=format&fit=crop&w=1498&q=80)", width: "100vw", height: "100vh",}}></div>
+            <div style={{background: "url(https://images.unsplash.com/photo-1466129646777-494b376a670c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80)", width: "100vw", height: "100vh",}}>
+              <h3 style={{color: "white"}}>Op-Eds</h3>
+            </div>
+            <div style={{background: "url(https://images.unsplash.com/photo-1507608869274-d3177c8bb4c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80)", width: "100vw", height: "100vh",}}>
+              <h3 style={{color: "white"}}>Love Actualized</h3>
+            </div>
+            <div>
+              <div style={{background: "url(https://images.unsplash.com/photo-1520634996521-d0985c84316a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjI0MX0&auto=format&fit=crop&w=1498&q=80)", width: "100vw", height: "100vh",}}>
+                <h3 style={{color: "white"}}>Columnists: Fall 2018</h3>
+              </div>
+              <div style={{background: "url(https://images.unsplash.com/photo-1520634996521-d0985c84316a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjI0MX0&auto=format&fit=crop&w=1498&q=80)", width: "100vw", height: "100vh",}}>
+                <h3 style={{color: "white"}}>Columnists: Spring 2019</h3>
+              </div>
+            </div>
+            <div style={{background: "url(https://images.unsplash.com/photo-1479030160180-b1860951d696?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)", width: "100vw", height: "100vh",}}>
+              <h3 style={{color: "white"}}>Discourse &amp; Debate</h3>
+            </div>
           </ScrollingNavBar>
       </React.Fragment>
     );
