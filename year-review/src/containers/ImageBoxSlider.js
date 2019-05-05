aab475c0492aa6a027494ffeb4712bf3e17db85d
ArsalaanAnsariDeveloper <aaa2325@columbia.edu>
2019-05-05 19:05:41 -0400

    Fixed centering on mobile for news

--- year-review/src/containers/ImageBoxSlider.js
+++ year-review/src/containers/ImageBoxSlider.js
@@ -177,7 +177,6 @@ class ImageBoxSlider extends Component {
 					<MobileArticleContainer key = {i}>
 						<Article title= {data.title} author={data.author} url = {data.url}
 							description={data.description} img_src={data.img_src} />
-						{article2}
 					</MobileArticleContainer>
 				)
 			})
