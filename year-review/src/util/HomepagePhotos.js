07747d57ce75db3f82695a703fd21cb8b567871a
Kevin Li <kevinl94303spam@gmail.com>
2019-05-07 15:02:36 -0400

    removed picture per katherine

--- year-review/src/util/HomepagePhotos.js
+++ year-review/src/util/HomepagePhotos.js
@@ -1,6 +1,5 @@
 const HomepagePhotos = [
     "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/TLYYTSDLYZBVFGHEZWGMOFYXU4.jpg",
-    "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/7GAXQSUWDJHIPAEK6VERGLCL7Y.jpg",
     "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/UFDU54CICZDJLI6NJ6NWXTJTG4.jpg",
     "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/LEDJIEOR7JGR3EXNOQ2I6UAXYE.jpg",
     "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/W7C3QDVUKBEFLDJC2LO27KXV6Q.jpg",
