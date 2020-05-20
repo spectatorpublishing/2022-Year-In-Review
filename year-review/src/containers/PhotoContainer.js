ec06f99b2cae2a074fd6f5fcd554c163072635b4
Carl Dobrovic <cadobrovic@gmail.com>
2020-05-20 15:01:32 -0400

    Fixed photos light boxes

--- year-review/src/containers/PhotoContainer.js
+++ year-review/src/containers/PhotoContainer.js
@@ -10,19 +10,59 @@ class PhotoContainer extends Component {
 	constructor(props){
 		super(props)
 		this.state={
-			lightboxActive: false,
-			lightboxIndex: 0
+			newsLightboxActive: false,
+			spectrumLightboxActive: false,
+			sportsLightboxActive: false,
+			eyeLightboxActive: false,
+			aneLightboxActive: false,
+			lightboxIndex: 0,
 		}
-		this.closeLightbox = this.closeLightbox.bind(this)
-		this.openLightbox = this.openLightbox.bind(this)
+		this.closeNewsLightbox = this.closeNewsLightbox.bind(this)
+		this.openNewsLightbox = this.openNewsLightbox.bind(this)
+		this.closeSpectrumLightbox = this.closeSpectrumLightbox.bind(this)
+		this.openSpectrumLightbox = this.openSpectrumLightbox.bind(this)
+		this.closeSportsLightbox = this.closeSportsLightbox.bind(this)
+		this.openSportsLightbox = this.openSportsLightbox.bind(this)
+		this.closeEyeLightbox = this.closeEyeLightbox.bind(this)
+		this.openEyeLightbox = this.openEyeLightbox.bind(this)
+		this.closeAnELightbox = this.closeAnELightbox.bind(this)
+		this.openAnELightbox = this.openAnELightbox.bind(this)
 	}
 
-	closeLightbox(){
-		this.setState({"lightboxActive": false})
+	closeNewsLightbox(){
+		this.setState({"newsLightboxActive": false})
 	}
-	openLightbox(i){
+	openNewsLightbox(i){
 		console.log('light index', i);
-		this.setState({"lightboxActive": true, "lightboxIndex": i})
+		this.setState({"newsLightboxActive": true, "lightboxIndex": i})
+	}
+	closeSpectrumLightbox(){
+		this.setState({"spectrumLightboxActive": false})
+	}
+	openSpectrumLightbox(i){
+		console.log('light index', i);
+		this.setState({"spectrumLightboxActive": true, "lightboxIndex": i})
+	}
+	closeSportsLightbox(){
+		this.setState({"sportsLightboxActive": false})
+	}
+	openSportsLightbox(i){
+		console.log('light index', i);
+		this.setState({"sportsLightboxActive": true, "lightboxIndex": i})
+	}
+	closeEyeLightbox(){
+		this.setState({"eyeLightboxActive": false})
+	}
+	openEyeLightbox(i){
+		console.log('light index', i);
+		this.setState({"eyeLightboxActive": true, "lightboxIndex": i})
+	}
+	closeAnELightbox(){
+		this.setState({"aneLightboxActive": false})
+	}
+	openAnELightbox(i){
+		console.log('light index', i);
+		this.setState({"aneLightboxActive": true, "lightboxIndex": i})
 	}
 
 	render() {
@@ -32,28 +72,58 @@ class PhotoContainer extends Component {
 		<GenericPanelLayout data = {this.props.data.Photo_Essays} />)}
 		
 		<ScrollingNavBar menuItems={this.props.data.sections}>
-			{this.props.data.items.map((data, i) => 
-			<React.Fragment>
-				<h3 style={{color: "white", Left: "1vw", padding: "1vh", "backgroundColor": "black"}}> {data.name}</h3>
-				<PhotoGrid data={data.articles} openLightbox={this.openLightbox}/>
-				{this.state.lightboxActive && <Lightbox
-					index={this.state.lightboxIndex} 
-					media={data.articles} 
-					authorLabel="photographer"
-					onClose={this.closeLightbox}>
-				</Lightbox>
-				}
-			</React.Fragment>
-			)}
+			<div>
+				<h3 style={{color: "white", Left: "1vw", padding: "1vh", "backgroundColor": "black"}}> {this.props.data.items[0].name}</h3>
+				<PhotoGrid data={this.props.data.items[0].articles} openLightbox={this.openNewsLightbox}/>
+			</div>
+			<div>
+				<h3 style={{color: "white", Left: "1vw", padding: "1vh", "backgroundColor": "black"}}> {this.props.data.items[1].name}</h3>
+				<PhotoGrid data={this.props.data.items[1].articles} openLightbox={this.openSpectrumLightbox}/>
+			</div>
+			<div>
+				<h3 style={{color: "white", Left: "1vw", padding: "1vh", "backgroundColor": "black"}}> {this.props.data.items[2].name}</h3>
+				<PhotoGrid data={this.props.data.items[2].articles} openLightbox={this.openSportsLightbox}/>
+			</div>
+			<div>
+				<h3 style={{color: "white", Left: "1vw", padding: "1vh", "backgroundColor": "black"}}> {this.props.data.items[3].name}</h3>
+				<PhotoGrid data={this.props.data.items[3].articles} openLightbox={this.openEyeLightbox}/>
+			</div>
+			<div>
+				<h3 style={{color: "white", Left: "1vw", padding: "1vh", "backgroundColor": "black"}}> {this.props.data.items[4].name}</h3>
+				<PhotoGrid data={this.props.data.items[4].articles} openLightbox={this.openAnELightbox}/>
+			</div>
+
 		</ScrollingNavBar>
-        {/* <PhotoGrid data={this.props.data.items.Photos} openLightbox={this.openLightbox}/>
-				{this.state.lightboxActive && <Lightbox
-					index={this.state.lightboxIndex} 
-					media={this.props.data.items.Photos} 
-					authorLabel="photographer"
-					onClose={this.closeLightbox}>
-				</Lightbox>
-				} */}
+			{this.state.newsLightboxActive && <Lightbox
+				index={this.state.lightboxIndex} 
+				media={this.props.data.items[0].articles} 
+				authorLabel="photographer"
+				onClose={this.closeNewsLightbox}>
+			</Lightbox>}
+			{this.state.spectrumLightboxActive && <Lightbox
+				index={this.state.lightboxIndex} 
+				media={this.props.data.items[1].articles} 
+				authorLabel="photographer"
+				onClose={this.closeSpectrumLightbox}>
+			</Lightbox>}
+			{this.state.sportsLightboxActive && <Lightbox
+				index={this.state.lightboxIndex} 
+				media={this.props.data.items[2].articles} 
+				authorLabel="photographer"
+				onClose={this.closeSportsLightbox}>
+			</Lightbox>}
+			{this.state.eyeLightboxActive && <Lightbox
+				index={this.state.lightboxIndex} 
+				media={this.props.data.items[3].articles} 
+				authorLabel="photographer"
+				onClose={this.closeEyeLightbox}>
+			</Lightbox>}
+			{this.state.aneLightboxActive && <Lightbox
+				index={this.state.lightboxIndex} 
+				media={this.props.data.items[4].articles} 
+				authorLabel="photographer"
+				onClose={this.closeAnELightbox}>
+			</Lightbox>}
       </React.Fragment>
     );
   }
